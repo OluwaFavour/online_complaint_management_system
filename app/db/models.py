eeee164644f03566ff43b952a3d10cb564740e98
Favour Olusayo <fsticks8187@gmail.com>
2024-08-15 17:41:19 +0100

    feat: Remove unique constraint on username field in User model

--- app/db/models.py
+++ app/db/models.py
@@ -101,7 +101,7 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
-    username: Mapped[str] = mapped_column(unique=True, index=True)
+    username: Mapped[str] = mapped_column(index=True)
     email: Mapped[str] = mapped_column(unique=True, index=True)
     hashed_password: Mapped[str] = mapped_column()
     is_active: Mapped[bool] = mapped_column(insert_default=True)
